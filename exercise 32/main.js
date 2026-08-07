8d761710567bca3da597589d773628890801386f
caano-geel7x <zenvision7xgmail.com>
2026-08-07 11:14:39 +0100

    submit exercise 32

---
+++ exercise 32/main.js
@@ -0,0 +1,11 @@
+const header = document.querySelector('#main-header');
+console.log(header);
+
+let hdh = document.querySelectorAll('.php');
+console.log(hdh);
+
+// var hdh2 = document.querySelector('.php')[1];
+// console.log(hdh2);
+
+var last = document.querySelector('div');
+console.log(last);
