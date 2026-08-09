8c308465fdad4a6c304bd610730f88acf9303f6e
caano-geel7x <zenvision7xgmail.com>
2026-08-09 10:05:51 +0100

    exercise33

---
+++ exercise 33/main.js
@@ -0,0 +1,9 @@
+function changebutton() {
+   const section1 = document.getElementById('section1');
+   section1.textContent = "First section";
+
+}
+function pro () {
+    const section2 = document.getElementById('section2');
+    section2.textContent = "Second section";
+}
