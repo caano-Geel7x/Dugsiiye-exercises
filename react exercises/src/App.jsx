e25907abda4a8b2327e0d065b6e04daa4d01c8c5
caano-geel7x <zenvision7xgmail.com>
2026-08-18 11:58:39 +0100

    Exercise 3: Dynamic UserCard with props

--- react exercises/src/App.jsx
+++ react exercises/src/App.jsx
@@ -2,14 +2,18 @@
 // App.jsx
 import UserCard from './exercises/exercise1';
 import Blog from './exercises/exercise2';
+import  UserCardEx3 from './exercises/exercise3';
 
 const App = () => {
   return (
     <div>
-      <UserCard />
+      <UserCardEx3  name="khadar" email = "khadar.ahmed@gmail.com"/>
+      <UserCardEx3  name="caanoGeel" email = "caanogeel@.gmail.com"/>
+      <UserCardEx3  name="khalif" email = "khalif@.gmail.com"/>
       <Blog />
     </div>
   );
 };
 
 export default App;
+
---
+++ react exercises/src/exercises/exercise3.jsx
@@ -0,0 +1,11 @@
+// UserCard.jsx
+const UserCardEx3 = ({name,email}) => {
+  return (
+    <div>
+      <h2>name:{name}</h2>
+      <p>email:{email}</p>
+    </div>
+  );
+};
+
+export default UserCardEx3;
